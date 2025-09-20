c514d3c5bb767095b871773a2375310542507e7d
zwezdinvladimir <31263958+zwezdinvladimir@users.noreply.github.com>
2025-09-20 12:43:53 +0300

    Implemented montgomery

---
+++ Modular/Montgomery.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include<cstdint>
+
+using u32 = uint32_t;
+using u64 = uint64_t;
+
+struct Montgomery {
+    u32 mod, mod_inv;
+
+    Montgomery(u32 mod) : mod(mod), mod_inv(1) {
+        for (int i = 0; i < 5; ++i) {
+            mod_inv *= 2 - mod * mod_inv;
+        }
+    }
+    u32 norm(u32 x) const {
+        return x < x - mod ? x : x - mod;
+    }
+    u32 transform(u32 x) const {
+        return ((u64)x << 32) % mod;
+    }
+    u32 reduce(u64 x) const {
+        u32 m = (u32(x) * mod_inv * u64(mod)) >> 32;
+        return norm((x >> 32) + mod - m);
+    }
+    u32 add(u32 x, u32 y) const {
+        return x + y >= mod ? x + y - mod : x + y;
+    }
+    u32 sub(u32 x, u32 y) const {
+        return x >= y ? x - y : x + mod - y;
+    }
+    u32 mul(u32 x, u32 y) const {
+        return reduce((u64)x * y);
+    }
+};
